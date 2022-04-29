7ffad88c453c24da9178eb1aa3d2b120d2ffb493
isefrog38 <isefroge38@gmail.com>
2022-04-29 11:44:54 +0300

    create types todolist

--- src/Redux-Store/tasks-reducer.ts
+++ src/Redux-Store/tasks-reducer.ts
@@ -52,32 +52,8 @@ const TaskSlice = createSlice({
         builder.addCase(addTodolistAC, (state, action) => {
             state[action.payload.todolistId] = [];
         });
-
-        // /*TASK ActionCreators*/
-        // builder.addCase(setTasksAC, (state, action: PayloadAction<{ tasks: Array<TaskTypeWithStatusEntity>, todolistId: string }>) => {
-        //     state[action.payload.todolistId] = action.payload.tasks;
-        // });
-        // builder.addCase(removeTaskAC, (state, action) => {
-        //      state[action.payload.todolistId].filter(t => t.id !== action.payload.taskId);
-        // });
-        // builder.addCase(addTaskAC, (state, action: PayloadAction<{ todolistId: string, task: TaskTypeWithStatusEntity }>) => {
-        //     state[action.payload.todolistId] = [action.payload.task, ...state[action.payload.todolistId]];
-        // });
-        // builder.addCase(updateTaskAC, (state, action: PayloadAction<{ taskId: string, todolistId: string, model: UpdateTaskModelType }>) => {
-        //     state[action.payload.todolistId].map(t => t.id === action.payload.taskId ? {...t, ...action.payload.model} : t);
-        // });
-        // builder.addCase(changeTaskEntityStatusAC, (state, action: PayloadAction<{ todolistId: string, taskId: string, entityStatus: RequestStatusType }>) => {
-        //     state[action.payload.todolistId].map(t => t.id === action.payload.taskId ? {...t, entityTaskStatus: action.payload.entityStatus} : t);
-        // });
     },
 });
-
-
-// export const setTasksAC = createAction<{ tasks: Array<TaskTypeWithStatusEntity>, todolistId: string }>("SET_TASKS");
-// export const removeTaskAC = createAction<{ taskId: string, todolistId: string }, "REMOVE_TASK">("REMOVE_TASK");
-// export const addTaskAC = createAction<{ todolistId: string, task: TaskTypeWithStatusEntity }>("ADD_TASK");
-// export const updateTaskAC = createAction<{ taskId: string, todolistId: string, model: UpdateTaskModelType }>("UPDATE_TASK");
-// export const changeTaskEntityStatusAC = createAction<{ todolistId: string, taskId: string, entityStatus: RequestStatusType }>("CHANGE_TASK_ENTITY_STATUS");
 
 
 export const tasksReducer = TaskSlice.reducer;
--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import {GetTasksResponse, TaskType, TodolistType, UpdateTaskModelType, ResponseType} from "../Types/TodolistTypes";
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
@@ -44,58 +45,4 @@ export const todolistsAPI = {
 }
 
 
-// types
-export type TodolistType = {
-    id: string
-    title: string
-    addedDate: string
-    order: number
-}
-export type ResponseType<D = {}> = {
-    resultCode: number
-    messages: Array<string>
-    fieldsErrors: Array<string>
-    data: D
-}
 
-
-export enum TaskStatuses {
-    New = 0,
-    InProgress = 1,
-    Completed = 2,
-    Draft = 3
-}
-
-export enum TaskPriorities {
-    Low = 0,
-    Middle = 1,
-    Hi = 2,
-    Urgently = 3,
-    Later = 4
-}
-
-export type TaskType = {
-    description: string
-    title: string
-    status: TaskStatuses
-    priority: TaskPriorities
-    startDate: string
-    deadline: string
-    id: string
-    todoListId: string
-    order: number
-    addedDate: string
-}
-export type UpdateTaskModelType = {
-    title?: string
-    description?: string
-    status?: TaskStatuses
-    priority?: TaskPriorities
-    startDate?: string
-    deadline?: string
-}
-type GetTasksResponse = {
-    error: string | null
-    totalCount: number
-    items: TaskType[]
-}
